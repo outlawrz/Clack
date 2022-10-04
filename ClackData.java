37337223b18bb2d9e11a93510fa6b16e4f68bbdd
Outlaw-desktop <outlawrz@clarkson.edu>
2022-10-04 18:31:06 -0400

    Commit to Clack

--- ClackData.java
+++ ClackData.java
@@ -2,12 +2,12 @@ import java.util;
 
 public class ClackData
 {
-    protected String username;
-    protected int type;
-    protected Date date;
-    public final CONSTANT_LISTUSERS=0;
+    protected String username; //Protected Variables in CLackData class protected variables can be referenced again in subclasses without get method
+    protected int type; //This is how we determine what action to take based on constants in the program.
+    protected Date date; //This creates an object that describes the date that the class is run
+    public final CONSTANT_LISTUSERS=0; //This is one of the constants used along with the type variable used to determine whta action to take
 
-    public ClackData(String username, int type)
+    public ClackData(String username, int type) //This constructor is used to create a ClackData object
     {
         this.username=username;
         this.type=type;
@@ -15,7 +15,7 @@ public class ClackData
     }
     public ClackData(int type)
     {
-        this("Anon",type);
+        this("Anon",type);//"anon stands for ananymous for when no username is given
     }
 
     public ClackData()
