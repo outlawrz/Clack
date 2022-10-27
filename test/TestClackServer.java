437cdf285167ded305202aaefd04ad17725ce9e1
Outlaw-desktop <outlawrz@clarkson.edu>
2022-10-27 17:12:57 -0400

    Commit to Clack

--- test/TestClackServer.java
+++ test/TestClackServer.java
@@ -3,7 +3,7 @@ public class TestClackServer
     public static void main(String args[])
     {
         ClackServer server1 = new ClackServer(463837);
-        ClackServer server2 = new ClackServer(52228);
+        ClackServer server2 = new ClackServer(-52228);
 
         System.out.println(server1.toString());
         System.out.println("Server 1 Hash Code: " + server1.hashCode());
